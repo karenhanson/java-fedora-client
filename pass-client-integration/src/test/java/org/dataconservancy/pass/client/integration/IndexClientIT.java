7b188574e813ccdeabe4443451802bdd1ea4e5fc
Karen Hanson <karenhanson77@gmail.com>
2018-04-24 10:04:23 -0400

    Add indexer to integration test docker

--- pass-client-integration/src/test/java/org/dataconservancy/pass/client/integration/IndexClientIT.java
+++ pass-client-integration/src/test/java/org/dataconservancy/pass/client/integration/IndexClientIT.java
@@ -15,11 +15,45 @@
  */
 package org.dataconservancy.pass.client.integration;
 
+import java.net.URI;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+import org.dataconservancy.pass.client.PassClient;
+import org.dataconservancy.pass.client.PassClientFactory;
+import org.dataconservancy.pass.model.Grant;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  *
  * @author Karen Hanson
  */
-public class IndexClientIT {
+public class IndexClientIT extends ClientITBase {
 
+    static {
+        if (System.getProperty("pass.fedora.baseurl") == null) {
+            System.setProperty("pass.fedora.baseurl", "http://localhost:8080/fcrepo/rest/");
+        }
+        if (System.getProperty("pass.elasticsearch.url") == null) {
+            System.setProperty("pass.elasticsearch.url", "http://localhost:9200");
+        }
+    }
+    
+    @Test
+    public void testMixedCaseAwardNumber() throws InterruptedException {
+        PassClient client = PassClientFactory.getPassClient();
+        
+        Grant grant = random(Grant.class, 1);
+        URI grantId = client.createResource(grant);
+        
+        TimeUnit.SECONDS.sleep(3);
+        URI foundGrantId1 = client.findByAttribute(Grant.class, "awardNumber", grant.getAwardNumber().toLowerCase());
+        assertEquals(grantId, foundGrantId1);
+
+        URI foundGrantId2 = client.findByAttribute(Grant.class, "awardNumber", grant.getAwardNumber().toUpperCase());
+        assertEquals(grantId, foundGrantId2);
+    }
 }
